45f5d42a996d93b4edfdc2e6b50340907972b5fe
anson <1483809252@qq.com>
2017-06-10 19:12:00 +0800

    correct path in login.html

--- route.py
+++ route.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 app.secret_key = 'whatisasecretkey'
 
 @app.route('/', methods=['GET'])
-def login():
-	return render_template('/login.html')
+def getLoginHTML():
+	return render_template('login.html')
 
 def isLogin():
 	return 'isLogin' in session and session['isLogin']
