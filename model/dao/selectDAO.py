5ce426b526beabba3def520b8be19f876ba521f0
iaxax <1483809252@qq.com>
2017-06-13 16:54:38 +0800

    修复selectDAO缩进问题

--- model/dao/selectDAO.py
+++ model/dao/selectDAO.py
@@ -12,13 +12,14 @@ class SelectDAO:
 		sql = "select count(*) from selection where student_id = %d"%(studentId)
 		cursor.execute(sql)
 		result = cursor.fetchone()
-        return result[0]
+    	return result[0]
 
 	#获取课程选课学生数量
+	@staticmethod
 	def getSelectStudentNum(courseId):
 		conn = Connection.getConnection()
 		cursor = conn.cursor()
 		sql = "select count(*) from selection where course_id = %d"%(courseId)
 		cursor.execute(sql)
 		result = cursor.fetchone()
-        return result[0]
+    	return result[0]
